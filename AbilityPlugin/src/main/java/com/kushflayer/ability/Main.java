402c49c88237461cbbe7a36ea5bf7c7ce7bfbaa0
kushflayer <wow971307@gmail.com>
2026-01-17 20:55:30 +0530

    Update Main.java

--- AbilityPlugin/src/main/java/com/kushflayer/ability/Main.java
+++ AbilityPlugin/src/main/java/com/kushflayer/ability/Main.java
@@ -6,13 +6,10 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getLogger().info("AbilityPlugin enabled!");
         this.getCommand("ability").setExecutor(new AbilityCommand(this));
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("AbilityPlugin disabled!");
     }
 }
-
