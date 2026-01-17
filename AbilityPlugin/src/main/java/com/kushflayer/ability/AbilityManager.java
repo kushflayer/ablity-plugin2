b02dc455f2e7acb65e7c4c2f1c91294aa387d7b6
kushflayer <wow971307@gmail.com>
2026-01-17 20:54:25 +0530

    Update AbilityManager.java

--- AbilityPlugin/src/main/java/com/kushflayer/ability/AbilityManager.java
+++ AbilityPlugin/src/main/java/com/kushflayer/ability/AbilityManager.java
@@ -2,11 +2,7 @@ package com.kushflayer.ability;
 
 public class AbilityManager {
 
-    // Here you can add ability logic, cooldowns, effects, etc.
-
     public void useAbility(String abilityName) {
-        // Placeholder method
-        System.out.println("Ability " + abilityName + " used!");
+        System.out.println("Ability " + abilityName + " used");
     }
 }
-
