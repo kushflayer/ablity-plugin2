95b9d53b766fe086993beadb56db3b26be7bf764
kushflayer <wow971307@gmail.com>
2026-01-17 20:40:31 +0530

    Update AbilityCommand.java

--- AbilityPlugin/src/main/java/com/kushflayer/ability/AbilityCommand.java
+++ AbilityPlugin/src/main/java/com/kushflayer/ability/AbilityCommand.java
@@ -1,1 +1,27 @@
+package com.kushflayer.ability;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class AbilityCommand implements CommandExecutor {
+
+    private final Main plugin;
+
+    public AbilityCommand(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("Only players can use this command!");
+            return true;
+        }
+
+        player.sendMessage("Ability command triggered! (Work in progress)");
+        return true;
+    }
+}
 
