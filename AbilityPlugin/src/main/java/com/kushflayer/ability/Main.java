f8db2df5ea3de9890e1d3e468ec5fddf8a8637ec
kushflayer <wow971307@gmail.com>
2026-01-17 20:39:52 +0530

    Update Main.java

--- AbilityPlugin/src/main/java/com/kushflayer/ability/Main.java
+++ AbilityPlugin/src/main/java/com/kushflayer/ability/Main.java
@@ -1,1 +1,18 @@
+package com.kushflayer.ability;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("AbilityPlugin enabled!");
+        this.getCommand("ability").setExecutor(new AbilityCommand(this));
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("AbilityPlugin disabled!");
+    }
+}
 
